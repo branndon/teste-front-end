80a277cf895476243811334a933c0a2c7b539b34
Branndon Arruda <arruda.branndon@gmail.com>
2018-09-01 10:26:25 -0300

    feat(Input Password): create component search input component

--- icasei/src/app/app.module.ts
+++ icasei/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputSearchComponent } from './shared/components/input-search/input-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputSearchComponent
   ],
   imports: [
     BrowserModule
